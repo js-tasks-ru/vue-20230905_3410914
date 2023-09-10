b902b4fcad877579897e409eb9adec9e09e45f1f
Pavel Borshchevsky <3410914@gmail.com>
2023-09-10 23:21:04 +0300

    01-task: VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,43 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import {createApp, defineComponent} from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const operations = {
+  sum: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b,
+}
+
+const App = defineComponent({
+  name: 'App',
+  data() {
+    return {
+      leftOperand: null,
+      rightOperand: null,
+      operandType: 'sum',
+      result: 0,
+    }
+  },
+  methods: {
+    calculate(event) {
+      if (this.leftOperand === null || this.rightOperand === null) {
+        return;
+      }
+      if (event) {
+        this.operandType = event.target.value;
+      }
+      let result = operations[this.operandType](this.leftOperand, this.rightOperand);
+      this.result = result === Infinity ? 'Cannot divide by zero' : result;
+    }
+  },
+  watch: {
+    leftOperand() {
+      this.calculate();
+    },
+    rightOperand() {
+      this.calculate();
+    },
+  },
+});
+
+const app = createApp(App);
+app.mount('#app');
