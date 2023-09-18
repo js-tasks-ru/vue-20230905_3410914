f9fa530be5a87293845840f23f64cc2710ea1cf6
Pavel Borshchevsky <3410914@gmail.com>
2023-09-18 06:43:49 +0300

    03-task

--- 03-sfc/02-UiTransitionGroupFade/components/UiTransitionGroupFade.vue
+++ 03-sfc/02-UiTransitionGroupFade/components/UiTransitionGroupFade.vue
@@ -17,30 +17,30 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 /* _transitions.css */
 
 .fade-list {
   position: relative;
 }
 
-.fade-list > * {
+.fade-list >>> * {
   opacity: 1;
   transition: opacity 0.3s ease-out;
 }
 
-.fade-list .fade-list-leave-active {
+.fade-list >>> .fade-list-leave-active {
   position: absolute !important;
   left: 0;
   right: 0;
 }
 
-.fade-list .fade-list-enter-from,
-.fade-list .fade-list-leave-to {
+.fade-list >>> .fade-list-enter-from,
+.fade-list >>> .fade-list-leave-to {
   opacity: 0;
 }
 
-.fade-list .fade-list-move {
+.fade-list >>> .fade-list-move {
   transition: transform 0.3s;
 }
 </style>
