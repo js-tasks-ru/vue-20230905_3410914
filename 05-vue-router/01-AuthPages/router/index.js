8d9fc8ea5d863644b9b93953fc4778af7d35cecd
Pavel Borshchevsky <BorshchevskyPA@pba.su>
2023-10-03 16:38:15 +0300

    05-01-AuthPages

--- 05-vue-router/01-AuthPages/router/index.js
+++ 05-vue-router/01-AuthPages/router/index.js
@@ -1,6 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import PageIndex from "../views/PageIndex";
+import PageLogin from "../views/PageLogin";
+import PageRegister from "../views/PageRegister";
 
 export const router = createRouter({
   history: createWebHistory('/05-vue-router/01-AuthPages'),
-  routes: [],
+  routes: [
+    {
+      name: 'index',
+      path: '/',
+      component: PageIndex
+    },
+    {
+      name: 'login',
+      path: '/login',
+      component: PageLogin
+    },
+    {
+      name: 'register',
+      path: '/register',
+      component: PageRegister
+    },
+  ],
 });
