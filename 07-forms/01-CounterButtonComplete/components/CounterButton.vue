6b516abeb778403c263a395330d2042a7e90a340
Pavel Borshchevsky <BorshchevskyPA@pba.su>
2023-10-10 12:35:16 +0300

    07-01-CounterButton

--- 07-forms/01-CounterButtonComplete/components/CounterButton.vue
+++ 07-forms/01-CounterButtonComplete/components/CounterButton.vue
@@ -1,9 +1,34 @@
 <template>
-  <button></button>
+  <button @click="increaseCounter">{{ count || counter }}</button>
 </template>
+
 
 <script>
 export default {
   name: 'CounterButton',
+
+  props: {
+    count: {
+      type: Number,
+      default: 0
+    }
+  },
+
+  data() {
+    return {
+      counter: 0,
+    }
+  },
+
+  methods: {
+    increaseCounter() {
+      this.counter++
+      this.$emit('update:count', this.counter)
+    }
+  },
+
+  mounted() {
+    this.counter = this.count
+  }
 };
 </script>
